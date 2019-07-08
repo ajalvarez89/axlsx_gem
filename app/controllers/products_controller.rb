a6d2070c15e121124b0ee914c5821ed4122c81e4
Alvaro Alvarez <aalvareza@energiasolarsa.com>
2019-07-08 17:21:50 -0500

    Adding format axlsx

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,22 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.order('created_at DESC')
+
+
+    respond_to do |format|
+      format.html
+      format.xlsx {
+        response.headers['Content-Disposition'] = 'attachment; filename="all_products.xlsx"'
+      }
+    end
+
+  end
+
+
+  private
+
+  
+  
+
+end
