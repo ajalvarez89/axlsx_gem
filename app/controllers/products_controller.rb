b8756e8ed4285b88546b6440d0efae8d835b4a3e
Alvaro Alvarez <aalvareza@energiasolarsa.com>
2019-07-17 16:34:38 -0500

    wednesday

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,21 +2,74 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.order('created_at DESC')
-
-
+    @total =  @products.sum(:price)
+    @prome = prom(@total , @products.count)
+    @hash = data
+   
     respond_to do |format|
       format.html
       format.xlsx {
-        response.headers['Content-Disposition'] = 'attachment; filename="all_products.xlsx"'
+        response.headers['Content-Disposition'] = 'attachment; filename="CostAnalysis.xlsx"'
       }
     end
+  end
 
+  def sales_document
+    @products = Product.order('created_at DESC')
+    @total =  @products.sum(:price)
+    @prome = prom(@total , @products.count)
+    respond_to do |format|
+      format.xlsx {
+        response.headers['Content-Disposition'] = 'attachment; filename="all_products.xlsx"'
+      }
+    end      
   end
 
 
   private
 
+  def prom (sum , count)
+      a = (sum / count)
+      return a
+  end 
   
-  
+  def data 
+    @h = {
+      :marca1 => {
+        :name => "ES8000",
+        :alumin =>{
+          :titulo => "ALUMINIO",
+          :tipo => "ALU-DURANAR",
+          :cant => 10,
+          :cost_unit => 2500
+        },
+        :mano1 => {
+          :cost => 25000,
+          :cant => 34
+        },
+        :transport1 => {
+          :cost => 3000,
+          :cant => 34
+        }
+      },
+      :marca2 => {
+        :name => "ES8000",
+        :alumin =>{
+          :tipo => "ALU-SILVER",
+          :cant => 20,
+          :cost_unit => 450
+        },
+        :mano1 => {
+          :cost => 5000,
+          :cant => 43
+        },
+        :transport1 => {
+          :cost => 200,
+          :cant => 60
+        }
+      }
+    }
+    return @h
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products, only: [:index]
+  resources :products, only: [:index,:sales_document]
+  get  'products/index',  to: 'products#index'
+  get  'products/sales_document/',  to: 'products#sales_document'
+  get 'customer_sales_documents/:id', to: 'customer_sales_documents#show', as: 'customer_sales_document'
+
 
   root 'products#index'
 end
